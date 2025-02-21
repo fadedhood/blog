578a8848bfd34b79194364a904ebe250defc75ab
Faded <anonfaded@pm.me>
2025-02-22 03:23:54 +0500

    Update navigation label in toc.js for clarity

--- static/js/toc.js
+++ static/js/toc.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <svg viewBox="0 0 24 24" width="18" height="18">
                     <path fill="currentColor" d="M3 9h14V7H3v2zm0 4h14v-2H3v2zm0 4h14v-2H3v2zm16 0h2v-2h-2v2zm0-10v2h2V7h-2zm0 6h2v-2h-2v2z"/>
                 </svg>
-                <span>[SYS::NAV]</span>
+                <span>[NEURAL::INDEX]</span>
             </div>
             <button class="toc-toggle" aria-label="Close">
                 <svg viewBox="0 0 24 24" width="18" height="18">
