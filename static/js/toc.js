152df96b4099d5cd19addb1b216e4d611ee3bac8
Faded <anonfaded@pm.me>
2025-02-22 03:21:33 +0500

    Update SVG paths in toc.js for enhanced icon design

--- static/js/toc.js
+++ static/js/toc.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     floatButton.className = 'toc-float-button';
     floatButton.innerHTML = `
         <svg viewBox="0 0 24 24" width="20" height="20">
-            <path fill="currentColor" d="M3 9h14V7H3v2zm0 4h14v-2H3v2zm0 4h14v-2H3v2zm16 0h2v-2h-2v2zm0-10v2h2V7h-2zm0 6h2v-2h-2v2z"/>
+            <path fill="currentColor" d="M4 18h11c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h8c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h11c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm17.3 7.88L17.42 12l2.88-2.88c.39-.39.39-1.02 0-1.41a.9959.9959 0 0 0-1.41 0l-3.59 3.59c-.39.39-.39 1.02 0 1.41l3.59 3.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z"/>
         </svg>
     `;
 
