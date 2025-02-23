5a34379b11cc0faa0b3bc277da68b3ff71ff2090
Faded <anonfaded@pm.me>
2025-02-23 20:47:42 +0500

    Update author information, add typing effect for title, and enhance blockquote styles

---
+++ static/js/author.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const titles = [
+      "CTF Writeups",
+      "InfoSec Insights",
+      "Somewhere Between 0s and 1s",
+      "Exploit Researcher",
+      "Cyber Security Enthusiast"
+    ];
+  
+    let index = 0;
+    let charIndex = 0;
+    const speed = 80;
+    const titleElement = document.querySelector(".author-title");
+  
+    function typeEffect() {
+      if (charIndex < titles[index].length) {
+        titleElement.innerHTML = titles[index].substring(0, charIndex + 1) + `<span class="cursor"></span>`;
+        charIndex++;
+        setTimeout(typeEffect, speed);
+      } else {
+        setTimeout(eraseEffect, 1200);
+      }
+    }
+  
+    function eraseEffect() {
+      if (charIndex > 0) {
+        titleElement.innerHTML = titles[index].substring(0, charIndex - 1) + `<span class="cursor"></span>`;
+        charIndex--;
+        setTimeout(eraseEffect, speed / 2);
+      } else {
+        index = (index + 1) % titles.length;
+        setTimeout(typeEffect, speed);
+      }
+    }
+  
+    typeEffect();
+});
